19cc3d82fa57097f056acaa414745ccf349b4d75
unknown <juanmgiraldo00@gmail.com>
2022-08-16 18:00:40 -0500

    Complete first quarkus lab

--- labconfigapp/src/main/java/co/juan/GreetingResource.java
+++ labconfigapp/src/main/java/co/juan/GreetingResource.java
@@ -1,16 +1,28 @@
 package co.juan;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Optional;
 
-@Path("/hello")
+@Path("/greeting")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return message + " " + name.orElse("world") + suffix;
     }
+
+    @ConfigProperty(name = "greeting.message")
+    String message;
+
+    @ConfigProperty(name = "greeting.suffix", defaultValue="!")
+    String suffix;
+
+    @ConfigProperty(name = "greeting.name")
+    Optional<String> name;
 }
