81b18a690dcfeed981fb0593a6a11546c117ddc2
unknown <juanmgiraldo00@gmail.com>
2022-08-16 20:32:52 -0500

    Add test implementation from the guide

--- labtestingapp/src/test/java/co/juan/GreetingResourceTest.java
+++ labtestingapp/src/test/java/co/juan/GreetingResourceTest.java
@@ -1,21 +1,24 @@
 package co.juan;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
+import java.util.UUID;
+
+import static io.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.is;
+import static io.restassured.RestAssured.given;
 
 @QuarkusTest
+@TestHTTPEndpoint(GreetingResource.class)
 public class GreetingResourceTest {
 
     @Test
     public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+        when().get()
+                .then()
+                .statusCode(200)
+                .body(is("hello"));
     }
-
 }
